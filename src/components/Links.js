70004f55d0f43ab9baf2ad4a4f7e88d8b035cb19
Grace Durant <gdurant34@gmail.com>
2022-10-11 17:54:01 -0700

    completed lab

---
+++ src/components/Links.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Links(props) {
+  // console.log(props.links);
+  
+    return (
+    <>
+        <h3>Links</h3>
+        <a href={props.github}> {props.github} </a>
+        <a href={props.linkedin}> {props.linkedin} </a>
+    </>
+  );
+}
+
+export default Links;
+
